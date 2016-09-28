524cfec00ae74a86a83b5826b59e6fdf6b3ca84d
Johnny Greco <jgreco@astro.princeton.edu>
2016-09-28 14:47:59 -0400

    nsigma option

--- lsstutils/imgproc/core.py
+++ lsstutils/imgproc/core.py
@@ -7,7 +7,7 @@ import lsst.afw.math as afwMath
 
 __all__ = ['smooth_gauss']
 
-def smooth_gauss(masked_image, sigma):
+def smooth_gauss(masked_image, sigma, nsigma=7.0):
     """
     Smooth image with a Gaussian kernel. 
 
@@ -17,13 +17,15 @@ def smooth_gauss(masked_image, sigma):
         Masked image object to be smoothed
     sigma : float
         Standard deviation of Gaussian
+    nsigma : float, optional
+        Number of sigma for kernel width
 
     Returns
     -------
     convolved_image : lsst.afw.image.imageLib.MaskedImageF
         The convolved masked image
     """
-    width = (int(sigma * 7 + 0.5) // 2) * 2 + 1 # make sure it is odd
+    width = (int(sigma*nsigma + 0.5) // 2)*2 + 1 # make sure it is odd
     gauss_func = afwMath.GaussianFunction1D(sigma)
     gauss_kern = afwMath.SeparableKernel(width, width, gauss_func, gauss_func)
     convolved_image = masked_image.Factory(masked_image.getBBox())
